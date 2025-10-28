106473968c95561c158771e88bcd99428ee0b378
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2025-10-28 18:37:40 +0600

    chore: initial project setup with basic structure

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import authRoute from './routes/auth.js';
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use('/api/auth', authRoute);
+app.get('/', (req, res) => {
+  res.send('AI Docstring Generator Backend Running...');
+});
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is listening on PORT ${PORT}`);
+});
