d47a9f078442ca3a9752198cd8d64893edb0fa68
Rahat Ahmed Khan <rahatofficial.29@gmail.com>
2025-10-28 23:22:11 +0600

    feat(user):implement UserRepo class with findEmail and createUser methods

---
+++ server/models/User.js
@@ -0,0 +1,8 @@
+export default class User{
+    constructor(id,name,email,password){
+        this.id=id,
+        this.name=name,
+        this.email=email,
+        this.password=password
+    }
+}
